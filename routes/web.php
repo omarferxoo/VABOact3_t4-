bca25eaf4dd47f83abb2d3b6c53a57e2fa9c3bcb
Omar Valencia <omarrutiliovalenciaborja@gmail.com>
2026-07-17 10:19:54 -0600

    Fix root redirect for subdirectory deployment

--- routes/web.php
+++ routes/web.php
@@ -3,5 +3,5 @@
 use App\Http\Controllers\CursoController;
 use Illuminate\Support\Facades\Route;
 
-Route::redirect('/', '/cursos');
+Route::get('/', fn () => redirect()->route('cursos.index'));
 Route::resource('cursos', CursoController::class);
